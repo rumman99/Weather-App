fe1db2b3dc1a0e37d1f7ef4c345cf51582a8241d
rumman99 <tasnimrumman369@gmail.com>
2023-11-13 04:32:27 +0600

    Done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+
+async function weatherApi(){
+    let city= document.getElementById('city').value;
+    let res= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    let api= await res.json();
+
+    return api;
+};
+
+
+
+document.getElementById('clickBtn').addEventListener('click', function(){
+    weatherApi().then(data => display(data));
+});
+
+function display(temp){
+    document.getElementById('city_name').innerText= temp.name;
+    document.getElementById('temp').innerText= temp.main.temp;
+    document.getElementById('weather').innerText= temp.weather[0].main;
+    ///icon///
+    document.getElementById('icon').src= `http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+}
+
+// function setValue(){
+
+// }
